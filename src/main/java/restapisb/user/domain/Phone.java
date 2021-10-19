81dea8bbc5d3ce0cf8dbd612daecb8a1d220011b
marcelo <marcelo.schell@intraway.com>
2021-10-19 17:36:21 -0300

    remove heads comments

--- src/main/java/restapisb/user/domain/Phone.java
+++ src/main/java/restapisb/user/domain/Phone.java
@@ -1,12 +1,5 @@
 /*
- * Copyright (C) Intraway Corporation - All Rights Reserved (2020)
- *
- * Unauthorized copying of this file, via any medium is strictly prohibited
- *
- * Proprietary and confidential
- *
- * This file is subject to the terms and conditions defined in file LICENSE.txt, which is part of this source code
- * package.
+
  */
 package restapisb.user.domain;
 
